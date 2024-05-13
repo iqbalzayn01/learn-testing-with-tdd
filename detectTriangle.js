2316d6a9ab5bc71f7fa5c1b7c4bb2345d96eeeff
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2024-05-13 16:49:11 +0700

    update: 1 passed

--- detectTriangle.js
+++ detectTriangle.js
@@ -1,13 +1,13 @@
 function detectTriangle(a, b, c) {
-  //   if (typeof a !== 'number') {
-  //     throw new Error('Input must be a number');
-  //   }
-  //   if (typeof b !== 'number') {
-  //     throw new Error('Input must be a number');
-  //   }
-  //   if (typeof c !== 'number') {
-  //     throw new Error('Input must be a number');
-  //   }
+  if (typeof a !== 'number') {
+    throw new Error('Input must be a number');
+  }
+  if (typeof b !== 'number') {
+    throw new Error('Input must be a number');
+  }
+  if (typeof c !== 'number') {
+    throw new Error('Input must be a number');
+  }
 }
 
 export default detectTriangle;
