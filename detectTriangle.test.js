db0f94a45010e856b0181ab214a8ae4abefd16b1
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2024-05-13 16:20:51 +0700

    update: failed test 1

---
+++ detectTriangle.test.js
@@ -0,0 +1,28 @@
+/**
+ * test scenarios for detectTriangle
+ *
+ * - detectTriangle function
+ *   - should throw error when input is not a number
+ *   - should throw error when input is not a positive number
+ *   - should throw error when input is triangle inequality violation
+ *   - should return "Segitiga Sama Sisi" when all input are equal
+ *   - should return "Segitiga Sama Kaki" when two input are equal
+ *   - should return "Segitiga Sembarang" when all input are different
+ */
+
+import { describe, it } from 'vitest';
+import detectTriangle from './detectTriangle';
+
+describe('detectTriangle function', () => {
+  it('should throw error when input is not a number', () => {
+    expect(() => detectTriangle('a', 2, 3)).toThrowError(
+      'Input must be a number'
+    );
+    expect(() => detectTriangle(1, 'b', 3)).toThrowError(
+      'Input must be a number'
+    );
+    expect(() => detectTriangle(1, 2, 'c')).toThrowError(
+      'Input must be a number'
+    );
+  });
+});
